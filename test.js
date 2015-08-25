eb39c898ff2bc5db75c180ccad9d0a14ca7f7c27
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-08-26 08:27:23 +0900

    add test

--- test.js
+++ test.js
@@ -16,4 +16,22 @@ describe('quick-sort', function() {
     assert.deepEqual(sort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
     assert.deepEqual(sort([0, 0, 0, 0, 0, -1]), [-1, 0, 0, 0, 0, 0]);
   });
+
+  it('should work with random non-sorted arrays', function() {
+    function makeRandomArray() {
+      var precision = 0;
+      var multiplier = 100;
+      var size = 100;
+      var result = [];
+      for (var i = size; i > 0; i--) {
+        result.push(parseFloat((Math.random() * multiplier).toFixed(precision)));
+      }
+      return result;
+    }
+    var array = makeRandomArray();
+    sort(array);
+    for (var i = 0; i < array.length - 1; i++) {
+      assert((array[i] <= array[i + 1]) === true);
+    }
+  });
 });
